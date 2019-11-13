f9d7bc9f1e991a71b72395a3e4c8a0bfd2a3f96d
Roman Wecker <roman.wecker@nintendo.de>
2019-11-13 08:44:10 +0100

    SCSS Support

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import printMe from "./print.js";
-import { cube } from './math.js';
+import { cube } from "./math.js";
+import './style.scss';
 
 function getComponent() {
-  return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-    const element = document.createElement('div');
+  return import(/* webpackChunkName: "lodash" */ "lodash")
+    .then(({ default: _ }) => {
+      const element = document.createElement("div");
 
-    element.innerHTML = [
-      'Hello webpack!',
-      '5 cubed is equal to ' + cube(5)
-    ].join('\n\n');
+      element.innerHTML = [
+        "Hello webpack!",
+        "5 cubed is equal to " + cube(5)
+      ].join("\n\n");
+      element.classList.add('body');
 
-    return element;
-
-    }).catch(error => 'An error occurred while loading the component');
+      return element;
+    })
+    .catch(error => "An error occurred while loading the component");
 }
 
 getComponent().then(component => {
   document.body.appendChild(component);
-})
+});
