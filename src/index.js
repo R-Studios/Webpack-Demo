52b5143102517b4196763747f43ae35af4afe89e
Roman Wecker <roman.wecker@nintendo.de>
2019-11-18 16:44:03 +0100

    Templates work with relative paths Modified style

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ var atomic = require('./templates/atomic.handlebars')
 
 document.addEventListener('DOMContentLoaded', function () {
   var div = document.createElement('div')
+
   div.innerHTML = atomic()
-  var styles = require.context('./templates')
+
+  var styles = require.context('./')
   styles.keys().forEach(function (key) {
     if (key.includes('.scss')) {
       styles(key)
     }
   })
+
   document.body.appendChild(div)
 })
